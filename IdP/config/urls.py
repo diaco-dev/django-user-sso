d977c89ab62852e84164594e58ae9f56070a0eb4
diaco <shilahassanabadi@gmail.com>
2025-09-27 16:08:42 +0330

    remove change to fastapi

--- IdP/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/account/', include('account.urls')),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('auth/', include('oidc.oidc_urls')),
-]
