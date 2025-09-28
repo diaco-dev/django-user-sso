21c86bb04b956f8e3c9bff8564b4a8009cd9b39f
diaco <shilahassanabadi@gmail.com>
2025-09-28 11:02:40 +0330

    config client

--- client/config/urls.py
+++ client/config/urls.py
@@ -1,10 +1,11 @@
-
-
+from django.urls import path,include
+from core.views import oauth_callback, login_redirect
 from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
-    path('auth/', include('sso_auth.urls')),
+     path('admin/', admin.site.urls),
+    path("login/", login_redirect, name="login"),
+    path("oauth/callback/", oauth_callback, name="oauth_callback"),
+    path('api/v1/task/', include('task.urls')),
+    path('api/v1/core/', include('core.urls')),
 ]
+
