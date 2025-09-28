d0f88846c0d4f1cc17040d1517c10afadd33b114
diaco <shilahassanabadi@gmail.com>
2025-09-28 11:04:52 +0330

    script idp user

--- IdP/create_user_client.py
+++ IdP/create_user_client.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from app.db import get_db, Base, engine
+from app.database import get_db, Base, engine
 from app.models import User, OAuth2Client
 
 
@@ -8,30 +8,30 @@ def create_test_user_and_client(db: Session):
     user = db.query(User).filter(User.username == "testuser").first()
     if not user:
         user = User(
-            username="testuser",
-            password_hash="testpass",  # در تولید از bcrypt استفاده کنید
-            email="test@example.com"
+            username="admin",
+            password_hash="adminadmin",  # در تولید از bcrypt استفاده کنید
+            email="admin@admin.com"
         )
         db.add(user)
         db.commit()
-        print("کاربر 'testuser' با موفقیت ایجاد شد.")
+        print("create user.")
     else:
-        print("کاربر 'testuser' قبلاً وجود دارد.")
+        print("existing user")
 
     # ایجاد کلاینت OAuth2
     client = db.query(OAuth2Client).filter(OAuth2Client.client_id == "client1").first()
     if not client:
         client = OAuth2Client(
-            client_id="client1",
-            client_secret="secret1",
+            client_id="client2",
+            client_secret="secret2",
             redirect_uris="http://localhost:8001/oauth/callback/",
             scopes="openid profile email"
         )
         db.add(client)
         db.commit()
-        print("کلاینت 'client1' با موفقیت ایجاد شد.")
+        print("create client")
     else:
-        print("کلاینت 'client1' قبلاً وجود دارد.")
+        print("existing client")
 
 
 def main():
