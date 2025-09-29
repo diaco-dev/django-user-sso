bd27cedc67f48d1983cebaffe7205544f62c28f2
diaco <shilahassanabadi@gmail.com>
2025-09-29 18:41:41 +0330

    init db

--- IdP/app/database.py
+++ IdP/app/database.py
@@ -6,7 +6,6 @@ engine = create_engine(
     settings.DATABASE_URL,
     echo=settings.DEBUG,  # Log SQL queries in debug mode
     pool_pre_ping=True,
-    connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
